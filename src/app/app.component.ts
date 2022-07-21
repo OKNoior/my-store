2933008b569ef0f68ca2e9ebdec71cb99321fe76
Oscar Ivan Ortiz Rojas <oior986@homtail.com>
2022-07-21 06:53:26 -0500

    property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'my-store';
+  name = 'Oscar';
+  age = 36;
+  image = 'https://i.pinimg.com/originals/ad/7e/6a/ad7e6a6cdcc499dd52077b4d83e6dd2b.jpg';
+  btnDisable = true;
+  person ={
+    name: 'Oscar',
+    age: 36,
+    avatar: 'https://i.pinimg.com/originals/ad/7e/6a/ad7e6a6cdcc499dd52077b4d83e6dd2b.jpg'
+  }
 }
