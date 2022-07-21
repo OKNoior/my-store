eefa271eff19f1fe510753d175adba9f151858ec
Oscar Ivan Ortiz Rojas <oior986@homtail.com>
2022-07-21 08:24:01 -0500

    property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,34 @@
 import { Component } from '@angular/core';
+import { Form } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   name = 'Oscar';
   age = 36;
   image = 'https://i.pinimg.com/originals/ad/7e/6a/ad7e6a6cdcc499dd52077b4d83e6dd2b.jpg';
-  btnDisable = true;
+  btnDisabled = true;
   person ={
     name: 'Oscar',
     age: 36,
     avatar: 'https://i.pinimg.com/originals/ad/7e/6a/ad7e6a6cdcc499dd52077b4d83e6dd2b.jpg'
+  }
+
+  toggleButton(){
+    this.btnDisabled = !this.btnDisabled;
+  }
+  increaseAge(){
+    this.person.age += 1;
+  }
+  onScroll(event: Event){
+    const element =event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+  changeName(event: Event){
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
   }
 }
